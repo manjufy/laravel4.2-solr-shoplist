2cc28eaa09b282eaf6c6cadc41a227685f64b61c
manju16832003 <manju16832003@gmail.com>
2015-01-26 01:43:38 +0800

    #Added admin and nginx config

---
+++ app/controllers/ManagerController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Hardware Shop
+ *
+ * @author Manjunath Reddy<manju16832003@gmail.com>
+ * @version 1.0
+ * @package Manager/Admin
+ *
+ */
+
+/**
+ * Class ManagerController
+ */
+class ManagerController extends BaseController{
+
+
+}
---
+++ app/controllers/manager/ManagerHomeController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Hardware Shop
+ *
+ * @author Manjunath Reddy<manju16832003@gmail.com>
+ * @version 1.0
+ * @package Manager/Admin
+ *
+ */
+
+/**
+ * Class ManagerController
+ */
+class ManagerHomeController extends ManagerController{
+
+    public function showIndex() {
+        return View::make('manager/index');
+    }
+}
